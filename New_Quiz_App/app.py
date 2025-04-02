0cb2f043bc322a2b577e6c585da66d5e81adce37
Artem <av3411261@gmail.com>
2025-04-02 21:05:25 +0300

    Added . gitignore and updated app.py

--- New_Quiz_App/app.py
+++ New_Quiz_App/app.py
@@ -3,6 +3,6 @@ import flask
 New_Quiz = flask.Blueprint(
   name = "New_Quiz",
   import_name = "New_Quiz_App",
-  template_folder = "templates",
-  static_folder = "static",
+  template_folder = "New_Quiz_App/templates",
+  static_folder = "New_Quiz_App/static",
 )
