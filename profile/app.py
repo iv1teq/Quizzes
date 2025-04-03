0c0fc1e2e8efcb5058851c7683828bf70182f4b6
Nikita Hodovanyj <nikitahodovanyj@MacBook-Pro-Nikita.local>
2025-04-03 19:15:20 +0300

    start

--- profile/app.py
+++ profile/app.py
@@ -3,6 +3,6 @@ import flask
 profile = flask.Blueprint(
     name= "profile" ,
     import_name = "profile",
-    template_folder= "profile/templates",
-    static_folder= "profile/static"
+    template_folder='templates',
+    static_folder='../static/profile.css'
 )
