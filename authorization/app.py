a52b78e180a0d38768e54d89c19eabcbebe74ca3
Viktor <horiunowiktor@gmail.com>
2025-04-03 17:52:20 +0300

    Changed file app.py

--- authorization/app.py
+++ authorization/app.py
@@ -3,5 +3,5 @@ authorization = flask.Blueprint(
     name = "authorization",
     import_name = "authorization",
     template_folder = "templates",
-    static_folder = "static",
+    static_folder = "../static/science_and_entertainment_quiz",
                                 )
