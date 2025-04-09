ee83f86bd9b32ac8ca9d8855a5ad74de3191b613
KirillKharlan <151565579+KirillKharlan@users.noreply.github.com>
2025-04-09 20:18:31 +0300

    third push

--- project/urls.py
+++ project/urls.py
@@ -1,11 +1,8 @@
-import home_app, history
+import home_app
 
 from .settings import project
 
 home_app.home.add_url_rule(rule= '/', view_func=home_app.show_home_page, methods = ['POST', 'GET'])
 
-history.history.add_url_rule(rule= '/history',view_func=history.show_history_page, methods = ['POST', 'GET'])
 
 project.register_blueprint(blueprint=home_app.home)
-
-project.register_blueprint(blueprint=history.history)
