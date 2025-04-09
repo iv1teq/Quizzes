91dc73884f38a4bce7061adda5a3f58d931bf3fd
Artem <av3411261@gmail.com>
2025-04-09 21:45:52 +0300

    Added New_Quiz_Settings page, and customized styles for it

--- New_Quiz_App/views.py
+++ New_Quiz_App/views.py
@@ -7,3 +7,7 @@ def render_new_quiz():
 def render_new_quiz_2():
     context = {'page': 'home'}
     return flask.render_template('New_Quiz_App_2.html', **context)
+
+def render_new_quiz_settigs():
+    context = {'page': 'home'}
+    return flask.render_template('New_Quiz_Settings.html', **context)
--- project/urls.py
+++ project/urls.py
@@ -1,10 +1,12 @@
-import home_app
+import home_app, New_Quiz_App
 
 from .settings import project
 
 
 home_app.home.add_url_rule(rule= '/', view_func=home_app.show_home_page, methods = ['POST', 'GET'])
-
+New_Quiz_App.New_Quiz.add_url_rule(rule= '/new-quiz', view_func=New_Quiz_App.render_new_quiz, methods = ['POST', 'GET'])
+New_Quiz_App.New_Quiz.add_url_rule(rule= '/new-quiz-2', view_func=New_Quiz_App.render_new_quiz_2, methods = ['POST', 'GET'])
+New_Quiz_App.New_Quiz.add_url_rule(rule= '/new-quiz-settings', view_func=New_Quiz_App.render_new_quiz_settigs, methods = ['POST', 'GET'])
 
 project.register_blueprint(blueprint=home_app.home)
-
+project.register_blueprint(blueprint=New_Quiz_App.New_Quiz)
