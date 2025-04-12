ec187186a8249c8994e24ffe1b64a4fd4ee2f5c8
Artem <av3411261@gmail.com>
2025-04-12 21:17:12 +0300

    Fixed the structure of static

--- home_app/app.py
+++ home_app/app.py
@@ -1,11 +1,8 @@
-import flask, os
-
-DIR = os.path.abspath(os.path.dirname(__file__))
+import flask
 
 home = flask.Blueprint(
     name='home_app',
     import_name='home_app',
     template_folder='templates',
-    static_folder=os.path.join(DIR, 'static'),
-    static_url_path='/static/home_app',
+    static_folder='../static/home_app'
 )
