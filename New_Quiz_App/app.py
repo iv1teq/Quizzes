1cfca67842e01285bdf1897ffee2981ce786f268
TimofeyRas <timka.dp.ua@gmail.com>
2025-04-12 21:26:45 +0300

    fixed static

---
+++ New_Quiz_App/app.py
@@ -0,0 +1,8 @@
+import flask
+
+New_Quiz = flask.Blueprint(
+  name = "New_Quiz",
+  import_name = "New_Quiz_App",
+  template_folder = "templates",
+  static_folder = "../static/new-quiz-app-static",
+)
