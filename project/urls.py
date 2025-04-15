cbb69a126ce744a68f101447bf6960443b06fc58
KirillKharlan <151565579+KirillKharlan@users.noreply.github.com>
2025-04-15 17:35:48 +0300

    five push

--- project/urls.py
+++ project/urls.py
@@ -4,4 +4,6 @@ from .settings import project
 
 home_app.home.add_url_rule(rule= '/', view_func=home_app.show_home_page, methods = ['POST', 'GET'])
 
+
+
 project.register_blueprint(blueprint=home_app.home)
