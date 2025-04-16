c4cd3b552afd0d0aa395b1a5c292ee4f105779f5
Nikita Hodovanyj <nikitahodovanyj@MacBook-Pro-Nikita.local>
2025-04-16 19:17:45 +0300

    doing static for profile

--- profile/views.py
+++ profile/views.py
@@ -1,6 +1,6 @@
 import flask
 
 def show_profile_page():
-    context = {'page': 'home'}
+    context = {'profile': 'home'}
     return flask.render_template(template_name_or_list="profile.html", **context )
    
