c63881440fc1f4c600879b7a5513ecff96c742b9
TimofeyRas <timka.dp.ua@gmail.com>
2025-04-20 21:39:29 +0300

    views.py

--- registration/views.py
+++ registration/views.py
@@ -1,5 +1,17 @@
 import flask
-
+from profile.models import User, db
 def show_page_registration():
     context = {'page': 'home'}
+    if flask.request.method == 'POST':
+        user = User(
+            name = flask.request.form['login'],
+            email = flask.request.form['email'],
+            password = flask.request.form['password'],
+            is_admin = flask.request.form['Teacher']
+        )
+        try:
+            db.session.add(user)
+            db.session.commit()
+        except Exception as e:
+            print(e)
     return flask.render_template(template_name_or_list='registration.html', **context)
