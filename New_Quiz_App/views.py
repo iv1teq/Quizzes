ce4e867db0982802e0d5afb44344675e72264b1e
Artem <av3411261@gmail.com>
2025-04-20 23:41:16 +0300

    Working on page styles, released form data transfer

--- New_Quiz_App/views.py
+++ New_Quiz_App/views.py
@@ -1,3 +1,6 @@
+from flask import request, render_template
+from .models import Quiz
+from project.settings import db
 import flask
 
 def render_new_quiz():
@@ -9,5 +12,20 @@ def render_new_quiz_2():
     return flask.render_template('New_Quiz_App_2.html', **context)
 
 def render_new_quiz_settigs():
+    if request.method == 'POST':
+        try:
+            quiz = Quiz(
+                name=request.form['name'],
+                json_test_data=request.form['json_test_data'], 
+                count_questions=int(request.form['count_questions']),
+                topic=request.form['topic'],
+                image=request.form['image'],  
+                description=request.form['description']
+            )
+            db.session.add(quiz)
+            db.session.commit()
+        except Exception as e:
+            print(e)
+
     context = {'page': 'home'}
-    return flask.render_template('New_Quiz_Settings.html', **context)
+    return render_template('New_Quiz_Settings.html', **context)
