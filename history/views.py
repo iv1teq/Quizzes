c93b481004a6a9efcc17cb9addece61a0aa57887
KirillKharlan <151565579+KirillKharlan@users.noreply.github.com>
2025-04-24 22:04:07 +0300

    two pages design

--- history/views.py
+++ history/views.py
@@ -3,4 +3,12 @@ import flask
 def show_history_page():
     
     return flask.render_template(template_name_or_list="history.html" )
+
+def show_qsr_page():
+    
+    return flask.render_template(template_name_or_list="quiz_student_result.html" )
+
+def show_qtr_page():
+    
+    return flask.render_template(template_name_or_list="quiz_teacher_result.html" )
    
