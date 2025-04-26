504a76aa6e4e8a69341b4b5013c326cd737c1139
Dmitriy <dmitriypechenyuk0@gmail.com>
2025-04-26 20:06:04 +0300

    commit before merge timofey branch

--- home_app/views.py
+++ home_app/views.py
@@ -1,5 +1,7 @@
-import flask
+import flask, flask_login
 
 def show_home_page():
-    context = {'page': 'home', 'is_auth': True}
+    print(flask_login.current_user.is_authenticated)
+
+    context = {'page': 'home', 'is_auth': flask_login.current_user.is_authenticated, 'name': flask_login.current_user.name}
     return flask.render_template('home.html', **context)
