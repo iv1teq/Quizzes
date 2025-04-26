0271754a60e8741236727b7c38bafa47c30cf937
Dmitriy <dmitriypechenyuk0@gmail.com>
2025-04-26 20:28:45 +0300

    commit before merge kirill branch

--- home_app/views.py
+++ home_app/views.py
@@ -1,7 +1,8 @@
 import flask, flask_login
 
 def show_home_page():
-    print(flask_login.current_user.is_authenticated)
-
-    context = {'page': 'home', 'is_auth': flask_login.current_user.is_authenticated, 'name': flask_login.current_user.name}
+    if flask_login.current_user.is_authenticated:
+        context = {'page': 'home', 'is_auth': flask_login.current_user.is_authenticated, 'name': flask_login.current_user.name}
+    else:
+        context = {'page': 'home', 'is_auth': flask_login.current_user.is_authenticated}
     return flask.render_template('home.html', **context)
