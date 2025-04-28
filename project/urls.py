6d00e722c7150c6540222617821f36466a06a52f
KirillKharlan <151565579+KirillKharlan@users.noreply.github.com>
2025-04-28 23:01:06 +0300

    design_4.0

--- project/urls.py
+++ project/urls.py
@@ -5,6 +5,4 @@ from .settings import project
 home_app.home.add_url_rule(rule= '/', view_func=home_app.show_home_page, methods = ['POST', 'GET'])
 
 
-
-
 project.register_blueprint(blueprint=home_app.home)
