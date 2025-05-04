4e43693c7158628d777a863fd8c7b12ac4d0b82c
Dmitriy <dmitriypechenyuk0@gmail.com>
2025-05-04 21:12:27 +0300

    merge origin/artem

--- home_app/views.py
+++ home_app/views.py
@@ -2,7 +2,7 @@ import flask, flask_login
 from New_Quiz_App.models import Quiz
 
 def show_home_page():
-    quizzes = Quiz.query.all()
+    # quizzes = Quiz.query.all()
     if flask_login.current_user.is_authenticated:
         context = {'page': 'home', 'is_auth': flask_login.current_user.is_authenticated, 'name': flask_login.current_user.name, 'quizzes': quizzes}
     else:
