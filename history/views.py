3ac6691b60223d650a1f4762611008fdf7634a8b
KirillKharlan <151565579+KirillKharlan@users.noreply.github.com>
2025-05-09 20:41:30 +0300

    profile correction

--- history/views.py
+++ history/views.py
@@ -1,14 +1,21 @@
+from flask_login import current_user
 import flask
 
 def show_history_page():
     
     return flask.render_template(template_name_or_list="history.html" )
+    context = {'page': 'history'}
 
 def show_qsr_page():
     
     return flask.render_template(template_name_or_list="quiz_student_result.html" )
+    context = {'page': 'history',
+               'name': current_user.name,
+                'email': current_user.email,
+                }
 
 def show_qtr_page():
     
     return flask.render_template(template_name_or_list="quiz_teacher_result.html" )
+    context = {'page': 'history'}
    
