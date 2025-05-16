c115febf7b9673c835261683ddac5f840d1998f3
KirillKharlan <151565579+KirillKharlan@users.noreply.github.com>
2025-05-16 13:12:06 +0300

    history models

---
+++ history/static/js/Quiz_Teacher_Result.js
@@ -0,0 +1,21 @@
+// $(function(){
+//     ;('.result-frame'){
+//         "quiz_id": "id",
+//         "user_id": "id",
+//         "answers": {
+//             "question_1": "true",
+//             "question_2": "true",
+//             "question_3": "false",
+//             "question_4": "true",
+//             "question_5": "true",
+//             "question_6": "false",
+//             "question_7": "false",
+//             "question_8": "true",
+//             "question_9": "false",
+//             "question_10": "true"
+//         }
+//         }
+//     }
+
+
+// )
